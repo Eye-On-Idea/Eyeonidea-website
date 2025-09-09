dab8717b2ff115da0df333cdd8d8e2d3326eb42a
Rune <git@eyeonidea.com>
2025-09-09 12:07:45 +0200

    Updated config
    Set up nuxtUI and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
+  modules: ["@nuxtjs/seo", "@nuxt/image", "@nuxt/ui"],
   css: ["~/assets/css/main.css"],
-  modules: ["@nuxtjs/seo", "@nuxt/image"],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
